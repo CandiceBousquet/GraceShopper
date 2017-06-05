3528e7b96ed779e14004d7d5fab088f030045fd0
Priya Murthy <prismm@gmail.com>
2017-06-05 18:23:50 -0400

    created api items route file and wrote basic routes

---
+++ server/api/items.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../db/models/item.js')
+
+router.get('/', (req, res, next) => {
+    Item.findAll({
+            include: {
+
+            }
+        })
+        .then(result => res.json(result))
+        .catch(next)
+})
+
+router.get('/:itemId', (req, res, next) => {
+    Item.findAll({
+
+        })
+        .then(result => res.json(result))
+        .catch(next)
+})
+
+
+module.exports = router
