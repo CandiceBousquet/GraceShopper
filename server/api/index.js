20328352282ee7976ebb1a4578e537b10d1b5fe2
Grant Herman <grantherman@Grants-MacBook-Pro.local>
2017-06-06 11:30:15 -0400

    created user routing for CRUD operations

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
+router.use('/user', require('./user'));
+
 router.use('*', (req, res, next) => {
 	const err = new Error('Page not found');
 	err.status = 404;
 	next(err);
 })
+
 
 module.exports = router;
---
+++ server/api/user.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const User = require('../db/models/user');
+
+
+
+
+router.get('/', (req,res,next) => {
+    User.findAll()
+    .then((users) => {
+        res.json(users);
+    })
+    .catch(next);
+})
+
+router.post('/', (req,res,next) => {
+    User.create(req.body)
+    .then((user) => {
+        res.json(user);
+    })
+    .catch(next);
+})
+
+router.get('/:id', (req,res,next) => {
+    User.findById(req.params.id)
+    .then((user) => {
+        res.json(user);
+    })
+    .catch(next);
+})
+
+router.put('/:id', (req,res,next) => {
+    User.update(req.body)
+    .then((user) => {
+        res.json(user);
+    })
+    .catch(next);
+})
+
+router.delete('/:id', (req,res,next) => {
+    User.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    .then((user) => {
+        res.json(user);
+    })
+    .catch(next);
+})
+
+
+
+
+
+
+module.exports = router;
