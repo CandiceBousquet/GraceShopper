ad4d2e3390794f766bacef83c549412e31923616
Priya Murthy <prismm@gmail.com>
2017-06-07 14:44:58 -0400

    first commit

--- server/api/cart.js
+++ server/api/cart.js
@@ -3,8 +3,8 @@ const Order = require('../db/models/order');
 const Inventory = require('../db/models/inventory');
 const notFound = new Error(404)
 const outOfStock = new Error(" Item is out of Stock ")
-
-///Will need to work on the express-sessions for when user not logged in
+const Item = require('../db/models/item')
+    ///Will need to work on the express-sessions for when user not logged in
 
 /* 
     1. Create new Cart / Order -- X
@@ -25,132 +25,137 @@ const outOfStock = new Error(" Item is out of Stock ")
     It will then add that item to the order
     And return that updated order
 */
-router.post('/:itemId', (req,res,next) => {
+router.post('/:itemId', (req, res, next) => {
     let currentOrder;
+    let currentItem;
     Order.findOrCreate({
-        where: {
-            orderId: req.session.orderId,
-            userId:req.session.userId,
-            submitted:false
-        }    
-    })
-    .then(order => {
-        req.session.orderId = order.id
-        currentOrder = order;
-        return Inventory.findItem(req.params.itemId)
-    })
-    .then(item => {
-        return item.decrementQuantity(1)
-    })
-    .then(itemDecremented => {
-        return currentOrder.setItem(req.params.itemId);
-    })
-    .then(updatedOrder => {
-        res.json(updatedOrder);
-    })
-    .catch(next)
+            where: {
+                id: req.session.orderId,
+                userId: req.session.userId,
+                submitted: false
+            }
+        })
+        .then(order => {
+            req.session.orderId = order[0].id;
+            currentOrder = order[0];
+            return Item.findById(req.params.itemId)
+        })
+        .then(item => {
+            currentItem = item;
+            return Inventory.findById(item.inventoryId)
+        })
+        .then(inventory => {
+            inventory.decrementQuantity(1)
+        })
+        .then(() => {
+            return currentOrder.addItem(currentItem);
+        })
+        .then(updatedOrder => {
+            res.json(updatedOrder);
+        })
+        .catch(next)
 })
 
 /*
     Removes item from order
 */
-router.delete('/:itemId', (req,res,next) => {
-     let currentOrder;
+router.delete('/:itemId', (req, res, next) => {
+    let currentOrder;
     Order.findOne({
-        where: {
-            orderId: req.session.orderId
-        }    
-    })
-    .then(order => {
-        if(!order){
-            next(notFound)
-        }else{
-            currentOrder = order;
-            return Inventory.findOne({
-               where: {
-                itemId: req.params.itemId
-               } 
-            })
-        }     
-    })
-    .then(item => {
-        return item.incrementQuantity(1);
-    })
-    .then(itemIncremented => {
-        return currentOrder.removeItem(req.params.itemId)
-    })
-    .then(updatedOrder => {
-        res.json(updatedOrder);
-    })
-    .catch(next)
+            where: {
+                orderId: req.session.orderId
+            }
+        })
+        .then(order => {
+            if (!order) {
+                next(notFound)
+            } else {
+                currentOrder = order;
+                return Inventory.findOne({
+                    where: {
+                        itemId: req.params.itemId
+                    }
+                })
+            }
+        })
+        .then(item => {
+            return item.incrementQuantity(1);
+        })
+        .then(itemIncremented => {
+            return currentOrder.removeItem(req.params.itemId)
+        })
+        .then(updatedOrder => {
+            res.json(updatedOrder);
+        })
+        .catch(next)
 })
 
 /*
     Submits order
 */
-router.put('/:orderId', (req,res,next) => {
+router.put('/:orderId', (req, res, next) => {
     Order.update({
-        submitted:true
-    },{
-        where:{
-            orderId: req.params.orderId
-        },
-        returning:true
-    })
-    .then(submittedOrder => {
-        if(!submittedOrder[0]){
-            next(notFound)
-        }else{
-            const order = submittedOrder[1][0];
-            req.session.orderId = null;
-            res.json(order)
-        }
-    })
-    .catch(next)
+            submitted: true
+        }, {
+            where: {
+                orderId: req.params.orderId
+            },
+            returning: true
+        })
+        .then(submittedOrder => {
+            if (!submittedOrder[0]) {
+                next(notFound)
+            } else {
+                const order = submittedOrder[1][0];
+                req.session.orderId = null;
+                res.json(order)
+            }
+        })
+        .catch(next)
 
 })
 
 /*
     Getting Users most recent order
 */
-router.get('/:userId', (req,res,next) => {
-   Order.findOne({
-       where:{
-           userId:req.params.id,
-           submitted:false
-       }
-   })
-   .then(cart => {
-       res.json(cart);
-   })
-   .catch(next);
+router.get('/:userId', (req, res, next) => {
+    Order.findOne({
+            where: {
+                userId: req.params.id,
+                submitted: false
+            }
+        })
+        .then(cart => {
+            res.json(cart);
+        })
+        .catch(next);
 })
 
 /*
     Get User's order history 
 */
-router.get('/:userId/history', (req,res,next) => {
+router.get('/:userId/history', (req, res, next) => {
     Order.findAll({
-        where:{
-            userId:req.params.userId,
-            submitted:true
-        }
-    })
-    .then(orderHistory => {
-        res.json(orderHistory)
-    })
+            where: {
+                userId: req.params.userId,
+                submitted: true
+            }
+        })
+        .then(orderHistory => {
+            res.json(orderHistory)
+        })
 })
 
-router.delete('/:orderId', (req,res,next) => {
+router.delete('/:orderId', (req, res, next) => {
     Order.destroy({
-        where:{
-            id:req.params.orderId
-        }
-    })
-    .then((deletedOrder) => {
-        res.json(deletedOrder)
-    })
-    .catch(next);
+            where: {
+                id: req.params.orderId
+            }
+        })
+        .then((deletedOrder) => {
+            res.json(deletedOrder)
+        })
+        .catch(next);
 })
 
 
--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ const dbStore = new SequelizeStore({ db });
 dbStore.sync();
 
 app.use(session({
-	secret: process.env.SESSION_SECRET || 'candice is cool',
-	store: dbStore,
-	resave: false,
-	saveUninitialized: false
+    secret: process.env.SESSION_SECRET || 'candice is cool',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false
 }));
 
 // passport makes it easier to identify users
@@ -37,18 +37,18 @@ app.use(passport.session());
 
 // stores user's id in the session store upon login
 passport.serializeUser((user, done) => {
-  try {
-    done(null, user.id);
-  } catch (err) {
-    done(err);
-  }
+    try {
+        done(null, user.id);
+    } catch (err) {
+        done(err);
+    }
 });
 
 // runs when a user has already initiated a session and we want to re-obtain user info from the db
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => done(null, user))
-    .catch(done);
+    User.findById(id)
+        .then(user => done(null, user))
+        .catch(done);
 });
 
 // routing
@@ -56,25 +56,22 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api', require('./api'));
 
 app.get('*', (req, res, next) => {
-	res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // error handling
 app.use('/', (err, req, res, next) => {
-	console.error(err);
-	console.error(err.stack);
-	const status = err.status || 500;
-	const message = err.message || "Internal server error";
-	res.send(message).status(status);
+    console.error(err);
+    console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message || "Internal server error";
+    res.send(message).status(status);
 });
 
 db.sync({
-	force: true
+    // force: true
 }).then(() => {
-	app.listen(1337, () => {
-		console.log('listening on port 1337');
-	});
+    app.listen(1337, () => {
+        console.log('listening on port 1337');
+    });
 }).catch(console.error.bind(this));
-
-
-
