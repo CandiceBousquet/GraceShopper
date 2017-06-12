b1bccdd5a854bf47fe3a8741be37934cab9d799d
Candice Bousquet <candice.bousquet@gmail.com>
2017-06-12 12:57:51 -0400

    switch sequelize to lower case

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,4 +1,4 @@
-const Sequelize = require('Sequelize');
+const Sequelize = require('sequelize');
 
 // if using Heroku Postgres, use process.env.DATABASE_URL
 const databaseUrl = (process.env.NODE_ENV === 'production') ? process.env.DATABASE_URL : "postgres://localhost:5432/GraceShopper";
