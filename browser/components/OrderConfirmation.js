1f3ba2b6e498e8141ad673ebdaf2386402ea7f58
Candice Bousquet <candice.bousquet@gmail.com>
2017-06-13 11:34:08 -0400

    remove order history from order confirm page

--- browser/components/OrderConfirmation.js
+++ browser/components/OrderConfirmation.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import OrderHistory from './OrderHistory';
 
-export default function({mostRecentOrder}){
+export default function(props){
     return(
         <div>
             <h1>Congrats you submitted an Order!</h1>
-            <OrderHistory cartHistory={mostRecentOrder} />
         </div>
     )
 }
