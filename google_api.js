19177e92dc1d18aee0cfcf4c55c78a458eea0bc4
CandiceBousquet <candice.bousquet@gmail.com>
2017-06-13 22:23:51 -0400

    Delete google_api.js

--- google_api.js
+++
@@ -1,6 +0,0 @@
-
-process.env.GOOGLE_CLIENT_ID 
-module.exports ={
-    clientID:'389656586426-mloc2lejrg1084bv4l9v7ikre7mipjkd.apps.googleusercontent.com',
-    clientSecret: 'MDz3wg0LIq2SCjO2wr-yBgQf'
-}
